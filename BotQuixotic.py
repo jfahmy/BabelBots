2c062407124657e6d16d340a1cfcbd1d2b9ea5a3
jfahmy <jacqueline.fahmy0.o@gmail.com>
2019-01-04 08:43:34 -0800

    Add URL for ebook and pass it into get request, tested and working

--- BotQuixotic.py
+++ BotQuixotic.py
@@ -6,15 +6,17 @@ import grpc
 import ProseAndBabel_pb2
 import ProseAndBabel_pb2_grpc
 
+file_location = 'http://www.gutenberg.org/cache/epub/996/pg996.html'
+
 def run():
  channel = grpc.insecure_channel('localhost:50050')
  stub = ProseAndBabel_pb2_grpc.ProseAndBabelStub(channel)
- response = stub.GetHaiku(ProseAndBabel_pb2.BabelRequest())
+ response = stub.GetHaiku(ProseAndBabel_pb2.BabelRequest(ask=file_location))
  print("A Haiku 4 U:")
  print(response.response)
- response = stub.GetBabel(ProseAndBabel_pb2.BabelRequest())
- print("Markovian Babel:")
- print(response.response)
+ # response = stub.GetBabel(ProseAndBabel_pb2.BabelRequest(ask=file_location))
+ # print("Markovian Babel:")
+ # print(response.response)
 
 if __name__ == '__main__':
   run()
